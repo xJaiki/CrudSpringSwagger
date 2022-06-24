6aa3036876a73cd6efa3b93b09398805cff7acf1
unknown <mariodimarinox@gmail.com>
2022-06-24 15:51:52 +0200

    Spring initializr

---
+++ src/main/java/com/jaiki/CRUDSpringSwagger/CrudSpringSwaggerApplication.java
@@ -0,0 +1,13 @@
+package com.jaiki.CRUDSpringSwagger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudSpringSwaggerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudSpringSwaggerApplication.class, args);
+	}
+
+}
