5efb2593563c98eab357ffebdd64133ce0b45ac7
unknown <mariodimarinox@gmail.com>
2022-06-29 09:35:42 +0200

    fixed getAge method in Customer class

--- src/main/java/com/jaiki/CRUDSpringSwagger/controller/CustomerController.java
+++ src/main/java/com/jaiki/CRUDSpringSwagger/controller/CustomerController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jaiki.CRUDSpringSwagger.model.Customer;
 import com.jaiki.CRUDSpringSwagger.repository.CustomerRepository;
 
 
